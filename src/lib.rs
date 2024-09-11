833d0c0412c59bd76c25081214f38b55de0804d2
Jonathan <jonathanwoollettlight@gmail.com>
2024-09-11 04:23:56 +0100

    Repeat and retry

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! [![Crates.io](https://img.shields.io/crates/v/tracing-assertions)](https://crates.io/crates/tracing-assertions)
 //! [![docs](https://img.shields.io/crates/v/tracing-assertions?color=yellow&label=docs)](https://docs.rs/tracing-assertions)
 //! [![codecov](https://codecov.io/gh/JonathanWoollett-Light/tracing-assertions/branch/master/graph/badge.svg?token=II1xtnbCDX)](https://codecov.io/gh/JonathanWoollett-Light/tracing-assertions)
-//! 
+//!
 //! An assertions framework for [tracing](https://docs.rs/tracing/latest/tracing/).
 //!
 //! Simpler and faster than the alternatives.
@@ -52,6 +52,7 @@
 //!
 
 #![warn(clippy::pedantic)]
+#![allow(clippy::enum_glob_use)]
 
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering::SeqCst;
@@ -63,11 +64,19 @@ use tracing::Subscriber;
 use tracing_subscriber::field::Visit;
 use tracing_subscriber::layer::Context;
 
-#[derive(Default, Clone)]
-pub struct Layer {
-    pass_all: Arc<AtomicBool>,
-    assertions: Arc<Mutex<Vec<Arc<InnerAssertion>>>>,
+/// The assertion layer.
+#[derive(Default, Clone, Debug)]
+pub struct Layer(Arc<InnerLayer>);
+
+/// The inner layer shared between assertions and the assertion layer.
+///
+/// You should probably not use this directly.
+#[derive(Default, Debug)]
+struct InnerLayer {
+    pass_all: AtomicBool,
+    assertions: Mutex<Vec<Arc<InnerAssertion>>>,
 }
+
 impl Layer {
     /// Creates a string matching assertion.
     ///
@@ -79,15 +88,19 @@ impl Layer {
             boolean: AtomicBool::new(false),
             assertion_type: AssertionType::Matches(s.into()),
         });
-        self.assertions
+        self.0
+            .assertions
             .lock()
             .unwrap()
             .push(inner_assertion.clone());
-        Assertion::One(self.pass_all.clone(), inner_assertion)
+        Assertion(AssertionWrapper::One {
+            assertion: inner_assertion.clone(),
+            asserter: self.0.clone(),
+        })
     }
     /// The inverse of [`Layer::disable`].
     pub fn enable(&self) {
-        self.pass_all.store(false, SeqCst);
+        self.0.pass_all.store(false, SeqCst);
     }
     /// Tells all assertions to pass.
     ///
@@ -95,11 +108,11 @@ impl Layer {
     /// test for debugging without needing to comment out all the
     /// assertions you added.
     pub fn disable(&self) {
-        self.pass_all.store(true, SeqCst);
+        self.0.pass_all.store(true, SeqCst);
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum AssertionType {
     Matches(String),
 }
@@ -112,13 +125,30 @@ impl std::fmt::Display for AssertionType {
     }
 }
 
+/// An assertion.
 #[derive(Debug, Clone)]
-pub enum Assertion {
-    And(Box<Assertion>, Box<Assertion>),
-    Or(Box<Assertion>, Box<Assertion>),
-    One(Arc<AtomicBool>, Arc<InnerAssertion>),
-    Not(Box<Assertion>),
+pub struct Assertion(AssertionWrapper);
+
+/// This exists since there is no way of making enum variants private.
+#[derive(Debug, Clone)]
+enum AssertionWrapper {
+    And {
+        lhs: Box<Assertion>,
+        rhs: Box<Assertion>,
+    },
+    Or {
+        lhs: Box<Assertion>,
+        rhs: Box<Assertion>,
+    },
+    One {
+        assertion: Arc<InnerAssertion>,
+        asserter: Arc<InnerLayer>,
+    },
+    Not {
+        assertion: Box<Assertion>,
+    },
 }
+
 impl Assertion {
     /// Evaluates the assertion.
     ///
@@ -129,15 +159,116 @@ impl Assertion {
     pub fn assert(&self) {
         assert!(bool::from(self), "{}", self.ansi());
     }
+    /// Create a new assertion with the same condition.
+    ///
+    /// ```
+    /// use tracing_subscriber::layer::SubscriberExt;
+    /// let asserter = tracing_assertions::Layer::default();
+    /// let registry = tracing_subscriber::Registry::default();
+    /// let subscriber = registry.with(asserter.clone());
+    /// let guard = tracing::subscriber::set_default(subscriber);
+    /// let one = asserter.matches("one");
+    /// tracing::info!("one");
+    /// one.assert();
+    /// let one2 = one.repeat();
+    /// (!&one2).assert();
+    /// tracing::info!("one");
+    /// one2.assert();
+    /// ```
+    ///
+    /// # Panics
+    ///
+    /// When the inner mutex is poisoned.
+    #[must_use]
+    pub fn repeat(&self) -> Self {
+        use AssertionWrapper::*;
+        let inner = match &self.0 {
+            One {
+                assertion,
+                asserter,
+            } => {
+                let new_assertion = Arc::new(InnerAssertion {
+                    boolean: AtomicBool::new(false),
+                    assertion_type: assertion.assertion_type.clone(),
+                });
+                asserter
+                    .assertions
+                    .lock()
+                    .unwrap()
+                    .push(new_assertion.clone());
+                One {
+                    assertion: new_assertion,
+                    asserter: asserter.clone(),
+                }
+            }
+            Not { assertion } => Not {
+                assertion: Box::new(assertion.repeat()),
+            },
+            And { lhs, rhs } => And {
+                lhs: Box::new(lhs.repeat()),
+                rhs: Box::new(rhs.repeat()),
+            },
+            Or { lhs, rhs } => Or {
+                lhs: Box::new(lhs.repeat()),
+                rhs: Box::new(rhs.repeat()),
+            },
+        };
+        Self(inner)
+    }
+
+    /// Resets the assertion.
+    ///
+    /// ```
+    /// use tracing_subscriber::layer::SubscriberExt;
+    /// let asserter = tracing_assertions::Layer::default();
+    /// let registry = tracing_subscriber::Registry::default();
+    /// let subscriber = registry.with(asserter.clone());
+    /// let guard = tracing::subscriber::set_default(subscriber);
+    /// let one = asserter.matches("one");
+    /// tracing::info!("one");
+    /// one.assert();
+    /// one.reset();
+    /// (!&one).assert();
+    /// tracing::info!("one");
+    /// one.assert();
+    /// ```
+    ///
+    /// # Panics
+    ///
+    /// When the inner mutex is poisoned.
+    pub fn reset(&self) {
+        use AssertionWrapper::*;
+        match &self.0 {
+            One {
+                assertion,
+                asserter,
+            } => {
+                if assertion.boolean.swap(false, SeqCst) {
+                    asserter.assertions.lock().unwrap().push(assertion.clone());
+                }
+            }
+            Not { assertion } => assertion.reset(),
+            And { lhs, rhs } | Or { lhs, rhs } => {
+                lhs.reset();
+                rhs.reset();
+            }
+        }
+    }
+
     fn ansi(&self) -> String {
-        match self {
-            Assertion::One(pass_all, x) => {
-                let is_true = if pass_all.load(SeqCst) {
+        use AssertionWrapper::*;
+
+        match &self.0 {
+            One {
+                assertion,
+                asserter,
+            } => {
+                let is_true = if asserter.pass_all.load(SeqCst) {
                     true
                 } else {
-                    x.boolean.load(std::sync::atomic::Ordering::SeqCst)
+                    assertion.boolean.load(std::sync::atomic::Ordering::SeqCst)
                 };
-                let str = format!("{:?}", x.assertion_type.to_string());
+                let str = format!("{:?}", assertion.assertion_type.to_string());
                 let out = if is_true {
                     ansi_term::Colour::Green.paint(str)
                 } else {
@@ -145,9 +276,9 @@ impl Assertion {
                 };
                 out.to_string()
             }
-            Assertion::And(lhs, rhs) => format!("({} && {})", lhs.ansi(), rhs.ansi()),
-            Assertion::Or(lhs, rhs) => format!("({} || {})", lhs.ansi(), rhs.ansi()),
-            Assertion::Not(not) => format!("!{}", not.ansi()),
+            And { lhs, rhs } => format!("({} && {})", lhs.ansi(), rhs.ansi()),
+            Or { lhs, rhs } => format!("({} || {})", lhs.ansi(), rhs.ansi()),
+            Not { assertion } => format!("!{}", assertion.ansi()),
         }
     }
 }
@@ -161,7 +292,9 @@ impl std::ops::Not for Assertion {
 impl std::ops::Not for &Assertion {
     type Output = Assertion;
     fn not(self) -> Self::Output {
-        Assertion::Not(Box::new(self.clone()))
+        Assertion(AssertionWrapper::Not {
+            assertion: Box::new(self.clone()),
+        })
     }
 }
 
@@ -174,7 +307,10 @@ impl std::ops::BitAnd for Assertion {
 impl std::ops::BitAnd for &Assertion {
     type Output = Assertion;
     fn bitand(self, rhs: Self) -> Self::Output {
-        Assertion::And(Box::new(self.clone()), Box::new(rhs.clone()))
+        Assertion(AssertionWrapper::And {
+            lhs: Box::new(self.clone()),
+            rhs: Box::new(rhs.clone()),
+        })
     }
 }
 impl std::ops::BitOr for Assertion {
@@ -186,22 +322,29 @@ impl std::ops::BitOr for Assertion {
 impl std::ops::BitOr for &Assertion {
     type Output = Assertion;
     fn bitor(self, rhs: Self) -> Self::Output {
-        Assertion::Or(Box::new(self.clone()), Box::new(rhs.clone()))
+        Assertion(AssertionWrapper::Or {
+            lhs: Box::new(self.clone()),
+            rhs: Box::new(rhs.clone()),
+        })
     }
 }
 
 impl From<&Assertion> for bool {
     fn from(value: &Assertion) -> Self {
-        match value {
-            Assertion::One(pass_all, x) => {
-                if pass_all.load(SeqCst) {
+        use AssertionWrapper::*;
+        match &value.0 {
+            One {
+                assertion,
+                asserter,
+            } => {
+                if asserter.pass_all.load(SeqCst) {
                     return true;
                 }
-                x.boolean.load(std::sync::atomic::Ordering::SeqCst)
+                assertion.boolean.load(std::sync::atomic::Ordering::SeqCst)
             }
-            Assertion::And(lhs, rhs) => bool::from(&**lhs) && bool::from(&**rhs),
-            Assertion::Or(lhs, rhs) => bool::from(&**lhs) || bool::from(&**rhs),
-            Assertion::Not(not) => !bool::from(&**not),
+            And { lhs, rhs } => bool::from(&**lhs) && bool::from(&**rhs),
+            Or { lhs, rhs } => bool::from(&**lhs) || bool::from(&**rhs),
+            Not { assertion } => !bool::from(&**assertion),
         }
     }
 }
@@ -211,8 +354,11 @@ impl From<Assertion> for bool {
     }
 }
 
+/// The inner assertion shared between assertions and the assertion layer.
+///
+/// You should probably not use this directly.
 #[derive(Debug)]
-pub struct InnerAssertion {
+struct InnerAssertion {
     boolean: AtomicBool,
     assertion_type: AssertionType,
 }
@@ -229,7 +375,7 @@ impl<S: Subscriber> tracing_subscriber::layer::Layer<S> for Layer {
         // TODO This is a stupid way to access the message, surely there is a better way to get the message.
         let mut message = String::new();
         event.record(&mut EventVisitor(&mut message) as &mut dyn Visit);
-        let mut assertions = self.assertions.lock().unwrap();
+        let mut assertions = self.0.assertions.lock().unwrap();
         let mut i = 0;
         while i < assertions.len() {
             let result = match &assertions[i].assertion_type {
@@ -338,6 +484,98 @@ mod tests {
         let two = asserter.matches("two");
         (!&two).assert();
         assert!(!bool::from(two));
+
+        drop(guard);
+    }
+
+    #[test]
+    fn repeat() {
+        let asserter = Layer::default();
+        let base_subscriber = Registry::default();
+        let subscriber = base_subscriber.with(asserter.clone());
+        let guard = tracing::subscriber::set_default(subscriber);
+
+        let one = asserter.matches("one");
+        let two = asserter.matches("two");
+        let or = &one | &two;
+        let and = &one & &two;
+        let not = !&one;
+
+        info!("one");
+        info!("two");
+
+        one.assert();
+        two.assert();
+        or.assert();
+        and.assert();
+        (!&not).assert();
+
+        let one2 = one.repeat();
+        let two2 = two.repeat();
+        let or2 = or.repeat();
+        let and2 = and.repeat();
+        let not2 = not.repeat();
+
+        (!&one2).assert();
+        (!&two2).assert();
+        (!&or2).assert();
+        (!&and2).assert();
+        (!(!&not2)).assert();
+
+        info!("one");
+        info!("two");
+
+        one2.assert();
+        two2.assert();
+        or2.assert();
+        and2.assert();
+        (!&not2).assert();
+
+        drop(guard);
+    }
+
+    #[test]
+    fn reset() {
+        let asserter = Layer::default();
+        let base_subscriber = Registry::default();
+        let subscriber = base_subscriber.with(asserter.clone());
+        let guard = tracing::subscriber::set_default(subscriber);
+
+        let one = asserter.matches("one");
+        let two = asserter.matches("two");
+        let or = &one | &two;
+        let and = &one & &two;
+        let not = !&one;
+
+        info!("one");
+        info!("two");
+
+        one.assert();
+        two.assert();
+        or.assert();
+        and.assert();
+        (!&not).assert();
+
+        one.reset();
+        two.reset();
+        or.reset();
+        and.reset();
+        not.reset();
+
+        (!&one).assert();
+        (!&two).assert();
+        (!&or).assert();
+        (!&and).assert();
+        (!(!&not)).assert();
+
+        info!("one");
+        info!("two");
+
+        one.assert();
+        two.assert();
+        or.assert();
+        and.assert();
+        (!&not).assert();
 
         drop(guard);
     }
